5f101df6605421a7b5b171e78a722e9a87f16407
ECLOUD3\dai_l <dai_lam88@hotmail.com>
2026-09-19 14:04:28 +1000

    [important]: leapdev nextjs take home [important]: Init base project v0.1.0

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+// @ts-expect-error CSS is processed by Next.js at build time.
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
